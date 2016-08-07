bd2b5ee25fad22922081ae97158045a32de36dfe
Muhammad Fahad Baig <hackslash@Muhammads-MacBook-Pro.local>
2016-08-07 12:20:49 +0500

    forgot to push vendor_cherry.mk

---
+++ vendor_cherry.mk
@@ -0,0 +1,19 @@
+# Vendor Files
+# Proprietary Blobs (it's much better if you extract the required files manually from your device)
+
+# Graphics
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/lib/egl/libGLES_mali.so:system/lib/egl/libGLES_mali.so \
+	$(LOCAL_PATH)/lib64/egl/libGLES_mali.so:system/lib64/egl/libGLES_mali.so \
+	$(LOCAL_PATH)/lib/libgralloc_ext.default.so:system/lib/libgralloc_ext.default.so \
+	$(LOCAL_PATH)/lib64/libgralloc_ext.default.so:system/lib64/libgralloc_ext.default.so \
+	$(LOCAL_PATH)/lib/libgralloc_ext.so:system/lib/libgralloc_ext.so \
+	$(LOCAL_PATH)/lib64/libgralloc_ext.so:system/lib64/libgralloc_ext.so \
+	$(LOCAL_PATH)/lib/libhwaps.so:system/lib/libhwaps.so \
+	$(LOCAL_PATH)/lib64/libhwaps.so:system/lib64/libhwaps.so \
+	$(LOCAL_PATH)/lib/hw/gralloc.hi6210sft.so:system/lib/hw/gralloc.hi6210sft.so \
+	$(LOCAL_PATH)/lib64/hw/gralloc.hi6210sft.so:system/lib64/hw/gralloc.hi6210sft.so \
+	$(LOCAL_PATH)/lib/hw/hwcomposer.hi6210sft.so:system/lib/hw/hwcomposer.hi6210sft.so \
+	$(LOCAL_PATH)/lib64/hw/hwcomposer.hi6210sft.so:system/lib64/hw/hwcomposer.hi6210sft.so \
+	$(LOCAL_PATH)/lib/libion.so:system/lib/libion.so \
+	$(LOCAL_PATH)/lib64/libion.so:system/lib64/libion.so
