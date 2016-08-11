9e1d0650cfabd6c817aad9fd72077e0c22795eb3
Muhammad Fahad Baig <muhammad.fb.79@gmail.com>
2016-08-12 00:13:01 +0500

    cherryk: add wifi libs

--- vendor_cherryk.mk
+++ vendor_cherryk.mk
@@ -14,3 +14,14 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/lib/hw/gralloc.hi6210sft.so:system/lib/hw/gralloc.hi6210sft.so \
 	$(LOCAL_PATH)/lib64/hw/gralloc.hi6210sft.so:system/lib64/hw/gralloc.hi6210sft.so \
 	$(LOCAL_PATH)/lib64/libion.so:system/lib64/libion.so
+
+# WiFi
+PRODUCT_COPY_FILES += \
+	$(LOCAL_PATH)/lib/libwpa_client_hisi.so:system/lib/libwpa_client_hisi.so \
+	$(LOCAL_PATH)/lib/libwifi_factory_test.so:system/lib/libwifi_factory_test.so \
+	$(LOCAL_PATH)/lib/libwifi_factory_test_hi110x.so:system/lib/libwifi_factory_test_hi110x.so \
+	$(LOCAL_PATH)/lib/libwifichrstat.so:system/lib/libwifichrstat.so \
+	$(LOCAL_PATH)/lib64/libwpa_client_hisi.so:system/lib64/libwpa_client_hisi.so \
+	$(LOCAL_PATH)/lib64/libwifi_factory_test.so:system/lib64/libwifi_factory_test.so \
+	$(LOCAL_PATH)/lib64/libwifi_factory_test_hi110x.so:system/lib64/libwifi_factory_test_hi110x.so \
+	$(LOCAL_PATH)/lib64/libwifichrstat.so:system/lib64/libwifichrstat.so
